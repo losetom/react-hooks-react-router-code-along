40ec634a52e3e3eadc1099a7f6ff9087a7ab89f8
Mike Loseto <losetom1998@gmail.com>
2022-09-06 16:42:24 -0400

    Completed Assingment

--- src/index.js
+++ src/index.js
@@ -1,16 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
+// #1. Import react-router functions 
+import { BrowserRouter as Router } from "react-router-dom";
+import App from "./components/App";
 
-function Home() {
-  return (
-    <div>
-      <h1>Home!</h1>
-    </div>
-  );
-}
 
-function App() {
-  return <Home />;
-}
-
-ReactDOM.render(<App />, document.getElementById("root"));
+// #2. Change so router is coordinating what is displayed 
+ReactDOM.render(
+  <Router>
+    <App />
+  </Router>,
+  document.getElementById("root")
+);
